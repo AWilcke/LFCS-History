7a67fc6e9264c5b0d996d489c7928f7fdb4ebdc2
AWilcke <arthur.wilcke@gmail.com>
2016-06-20 14:02:09 +0100

    fixed first scaper (I think) and wrote second one

--- lfcs_scraping/lfcs_scraping/spiders/88mph.py
+++ lfcs_scraping/lfcs_scraping/spiders/88mph.py
@@ -2,7 +2,7 @@ import scrapy
 from lfcs_scraping.items import PersonItem
 import re
 
-class FirstTime(scrapy.Spider):
+class First(scrapy.Spider):
     name = "first"
     start_urls = [
             'http://web.archive.org/web/20160615173215/http://wcms.inf.ed.ac.uk/lfcs/people'
@@ -12,10 +12,10 @@ class FirstTime(scrapy.Spider):
 
         for name in response.xpath('//a/strong/u | //a/strong[not(u)] | //strong/a[not(strong)]'):
             try:
-                m = re.search('(.+) (\S+)', name.xpath('text()').extract()[0])
+                m = re.search('([^\d]+) (\S+)', name.xpath('text()').extract()[0])
             #if fails, it's because there was no text to extract, was a false positive
             except:
-                pass
+                m = None
 
             if m:
                 item = PersonItem()
@@ -25,11 +25,33 @@ class FirstTime(scrapy.Spider):
                 item['first'] = m.group(1)
                 item['year'] = response.xpath('//tr[@class="y"]/td[@class="c"]/text()').extract()[0]
                 yield item
-            #if no match then it isnt a name
-            else:
-                pass
         
         nextpage = response.xpath('//tr[@class="y"]/td[@class="b"]/a/@href').extract()
         if nextpage:
             yield scrapy.Request(response.urljoin(nextpage[0]), callback=self.parse)
- 
+
+class Second(scrapy.Spider):
+    name = "second"
+    start_urls = [
+            "http://web.archive.org/web/20070611175309/http://www.lfcs.inf.ed.ac.uk/people/"
+            ]
+
+    def parse(self, response):
+        yield
+        for name in response.xpath("//div[not(@id='wm-ipp')]//table//a"):
+            
+            try:
+                m = re.search('([^\d]+) (\S+)', re.sub('\xa0', ' ', name.xpath("text()").extract()[0]))
+            except:
+                m = None
+            if m:
+                item = PersonItem()
+                item['role'] = name.xpath("preceding::h3[1]/text()").extract()
+                item['last'] = m.group(2)
+                item['first'] = m.group(1)
+                item['year'] = response.xpath('//tr[@class="y"]/td[@class="c"]/text()').extract()[0]
+                yield item
+        
+        nextpage = response.xpath('//tr[@class="y"]/td[@class="b"]/a/@href').extract()
+        if nextpage:
+            yield scrapy.Request(response.urljoin(nextpage[0]), callback=self.parse)
